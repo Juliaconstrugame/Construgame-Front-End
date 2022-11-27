8c159b37d5fe5ca0e12251a380e41434aa95f663
flavioguisantos <flavio.guilherme@outlook.com.br>
2022-11-27 02:02:48 -0300

    fix: ajuste em head

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout({ children, title }) {
                 <title>
                     {`${title} | Construgame`}
                 </title>
-                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
                 <link rel="shortcut icon" href="/favicon.ico" />
 
             </Head>
